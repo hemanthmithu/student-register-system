7626fa71b749d577fe68a6a430bc03a31878175d
hemanthmithu <hemanthmithu@gamil.com>
2026-08-02 23:46:46 +0530

    Addes view page

--- root.py
+++ root.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def login():
     return render_template('login.html')
-
 
 
 @app.route('/register')
@@ -16,8 +13,7 @@ def register():
     return render_template('register.html')
 
 
-
-@app.route('/dashboard', methods=['GET','POST'])
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
 
     if request.method == "POST":
@@ -27,27 +23,34 @@ def dashboard():
         semester = request.form['semester']
         suggestion = request.form['suggestion']
 
-
         conn = sqlite3.connect("students.db")
         cursor = conn.cursor()
-
 
         cursor.execute("""
         INSERT INTO students(name, course, semester, suggestion)
         VALUES (?, ?, ?, ?)
-        """,
-        (name, course, semester, suggestion))
-
+        """, (name, course, semester, suggestion))
 
         conn.commit()
         conn.close()
 
-
         return "Student details submitted successfully!"
-
 
     return render_template('dashboard.html')
 
+
+@app.route('/view')
+def view():
+
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM students")
+    records = cursor.fetchall()
+
+    conn.close()
+
+    return render_template("view.html", records=records)
 
 
 if __name__ == "__main__":
